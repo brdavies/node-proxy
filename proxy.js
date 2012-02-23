c66998160f64ee5730f24c4c552fbb8202e1cb03
Ben Davies <brdavies@gmail.com>
2012-02-23 14:14:41 +1000

    Cleaned up documentation.

--- proxy.js
+++ proxy.js
@@ -6,10 +6,22 @@ var util = require('util'),
 
 /**
  * Start the proxy server.
+ *
+ * @param[in] routes
+ *     Routes table passed directly to package 'http-proxy'. For example:
+ *     {
+ *         hostnameOnly: true,
+ *         router: {
+ *           'foo.com': '127.0.0.1:8001',
+ *           'bar.com': '127.0.0.1:8002'
+ *         },
+ *         forward: {
+ *           port: 9000,
+ *           host: 'staging.com'
+ *         }
+ *     }
  */
 exports.run = function(routes) {
-
-    var arg = require('./args');
 
     /**
      * Table of command line options. */
@@ -39,16 +51,6 @@ that port.\
     /**
      * Current version of this application. */
     var version = "1.0.0";
-
-    /* Use commander.js to parse command line arguments. */
-    var args = arg.parse(version, usage, options, process.argv);
-
-    if ((args.port < 1024) && (!args.user)) {
-        /* If running on a privileged port a user must be specified so the
-         * application can drop to that user after opening the port. */
-        app_error('User must be specified for port ' + args.port + ' (see --user).');
-        process.exit(1);
-    }    
 
     /**
      * Helper function to print error messages.
@@ -109,10 +111,21 @@ that port.\
         util.puts('\nHTTP Proxy listening on port ' + args.port.toString().yellow);
     };
 
+    /**
+     * HTTP server. Will only be valid for workers. */
     var server;
+
+    /**
+     * Total number of workers online. */
     var worker_count = 0;
+
+    /**
+     * A hash of all known workers, indexed by the worker's PID. */
     var workers = {};
 
+    /**
+     * Start a single worker. This function should only be called from the
+     * master. */
     var worker_start = function() {
 
         var worker = cluster.fork();
@@ -122,20 +135,29 @@ that port.\
         worker.on('message', function(msg) {
             if (msg.cmd == 'ready') {
                 worker_count++;
-                app_info(
-                    "Worker " + worker_count +
-                        " of " + args.workers +
-                        " online.");
+                app_info("Worker " + worker_count +
+                         " of " + args.workers +
+                         " online.");
                 if (worker_count == args.workers) {
+                    /* Print information about the proxy server. */
                     app_status();
 
                     /* Drop privileges. */
-                    process.setuid(args.user);
+                    if (args.user ) {
+                        process.setuid(args.user);
+                    }
                 }
             }
         });
     };
 
+    /**
+     * Start the application. This function can be called from the master or
+     * worker.
+     *
+     * @pre
+     *     Global 'args' has been initialised.
+     */
     var app_start = function() {
 
         if ((args.workers > 1) && (cluster.isMaster)) {
@@ -151,6 +173,9 @@ that port.\
             });
 
         } else {
+            /* This is either a worker, or the application has been configured
+             * to run without workers. Start the proxy server and drop the
+             * privileges of the application once the server is listening. */
             server = httpProxy.createServer(routes);
 
             server.on('error', function(error) {
@@ -160,23 +185,36 @@ that port.\
                     app_error(error);
                 }
             });
-            
+
             server.listen(args.port, function() {
                 if (args.user) {
                     /* Drop privileges. */
                     process.setuid(args.user);
                 }
                 if (args.workers <= 1) {
-                    /* There are no child processes (workers), print some useful
-                     * information. */
+                    /* There are no workers (the master is the proxy), print
+                     * some useful information about the proxy server. */
                     app_status();
                 } else {
+                    /* Let the master know the proxy server is up and
+                     * running. */
                     process.send({ cmd: 'ready' });
                 }
             });
         }
     };
 
+    /**
+     * Function to free resources, and (in the case of the master) terminate all
+     * child workers (which aren't automatically terminated when the parent
+     * process dies).
+     *
+     * @param[in] status
+     *     Status code to exit with. 0 for success, non-zer for error.
+     * @param[in] signal
+     *     An optional string which describes the OS signal which caused the
+     *     application to terminate.
+     */
     var app_exit = function(status, signal) {
         if (server) {
             server.close();
@@ -184,30 +222,40 @@ that port.\
         }
         if (cluster.isMaster) {
             console.log("");
+            /* Kill all child workers. */
             for (var pid in workers) {
                 if (!workers.hasOwnProperty(pid)) {
                     continue;
                 }
-                try {                    
-                    app_info("Kill child " + pid);
-                    process.kill(pid);
-                } catch (e) {
-                    console.log(e);
-                }
+                app_info("Kill child " + pid);
+                process.kill(pid);
             }
             if (signal) {
                 signal = " (" + signal + ")";
             }
-            
+
             app_info("Exit" + signal + " with status " + status);
         }
-        
+
         process.exit(status);
     };
 
+    /* Catch signals to clean up resources and for workers to be explicitly
+     * terminated (they won't die automatically when the parent dies). */
     process.on("SIGKILL", function() { app_exit(0, "SIGKILL"); });
     process.on("SIGTERM", function() { app_exit(0, "SIGTERM"); });
     process.on("SIGINT", function() { app_exit(0, "SIGINT"); });
+
+    /* Parse command line arguments. */
+    var args = require('./args').parse(version, usage, options, process.argv);
+
+    /* Argument check. */
+    if ((args.port < 1024) && (!args.user)) {
+        /* If running on a privileged port a user must be specified so the
+         * application can drop to that user after opening the port. */
+        app_error('User must be specified for port ' + args.port + ' (see --user).');
+        process.exit(1);
+    }
 
     app_start();
 };
